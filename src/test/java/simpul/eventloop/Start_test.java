33d76cd43dcdff0f2da8a233f56ff9cfd1bea327
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-02-14 18:49:02 +0100

    skeleton

---
+++ src/test/java/simpul/eventloop/Start_test.java
@@ -0,0 +1,15 @@
+package simpul.eventloop;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class Start_test {
+    @Test
+    public void aFastTest() {
+        assertThat("a test", equalTo("a test"));
+    }
+
+}
